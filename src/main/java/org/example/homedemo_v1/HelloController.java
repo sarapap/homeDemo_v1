39bbc1ac8015893a354cb291fb05ffb04703aeda
sarapappila <sara.pappila@metropolia.fi>
2024-10-28 16:43:44 +0200

    init

--- src/main/java/org/example/homedemo_v1/HelloController.java
+++ src/main/java/org/example/homedemo_v1/HelloController.java
@@ -8,8 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -22,6 +23,12 @@ public class HelloController {
     private Button button2;
     @FXML
     private Button button3;
+    @FXML
+    private Label nameText;
+    @FXML
+    private Label date;
+    @FXML
+    private Label infoLabel;
 
     public void initialize(){
         //ResourceBundle bundle = ResourceBundle.getBundle("bundle2");
@@ -33,16 +40,37 @@ public class HelloController {
 
     public void setEN(ActionEvent actionEvent) {
         Locale l = new Locale("EN");
+        nameText.setText("Sara");
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, l);
+        String formattedDate = dateFormat.format(new Date(System.currentTimeMillis()));
+        date.setText(formattedDate);
+
+        infoLabel.setText(l.getDisplayLanguage());
+
         loadView(l);
     }
 
     public void setIR(ActionEvent actionEvent) {
         Locale l = new Locale("IR");
+        nameText.setText("Sara");
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, l);
+        String formattedDate = dateFormat.format(new Date(System.currentTimeMillis()));
+        date.setText(formattedDate);
+
+        infoLabel.setText(l.getDisplayLanguage());
+
         loadView(l);
     }
 
     public void setJP(ActionEvent actionEvent) {
         Locale l = new Locale("JP");
+        nameText.setText("Sara");
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, l);
+        String formattedDate = dateFormat.format(new Date(System.currentTimeMillis()));
+        date.setText(formattedDate);
+
+        infoLabel.setText(l.getDisplayLanguage());
+
         loadView(l);
     }
 
@@ -53,10 +81,11 @@ public class HelloController {
             Parent root = fxmlLoader.load();
             Stage s = (Stage) button1.getScene().getWindow();
             s.setScene(new Scene(root));
+            HelloController controller = fxmlLoader.getController();
+            controller.nameText.setText(nameText.getText());
+            controller.date.setText(date.getText());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-
 }
